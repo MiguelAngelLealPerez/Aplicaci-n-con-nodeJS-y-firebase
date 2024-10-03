185505534d78d60611e1171f3583a31445296c3e
MiguelAngelLealPerez <angellp.ti23@utsjr.edu.mx>
2024-10-02 19:20:06 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const usuariosRutas=require("./rutas/rutasUsuarios");
+const productosRutas=require("./rutas/rutasProductos");
+const ventasRutas=require("./rutas/rutasVentas");
+
+const app=express();{
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+app.use("/usuarios",usuariosRutas);
+app.use("/productos",productosRutas);
+app.use("/ventas",ventasRutas);
+}
+
+const port=process.env.PORT || 3000;
+
+app.listen(port, ()=>{
+    console.log("Servidor en http://localhost:"+port);
+});
